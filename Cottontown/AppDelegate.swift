7efe0be183521562dc09cff5bff36c6618975734
Ron Hildreth <ron@hildrethus.com>
2016-03-02 21:56:10 -0500

    debug getup problem

--- Cottontown/AppDelegate.swift
+++ Cottontown/AppDelegate.swift
@@ -5,7 +5,7 @@
 //  Created by Ron Hildreth on 12/12/15.
 //  Copyright © 2015 Tappdev.com. All rights reserved.
 //
-// test commit problem
+// test commit problem - gitup didn't commit
 
 import UIKit
 import CoreLocation
