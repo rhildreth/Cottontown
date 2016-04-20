7a915d1c276e4fb775c88a4d0377d11a1d45debf
Ron Hildreth <ron@hildrethus.com>
2016-04-20 14:22:01 -0400

    Switched from YouTube to local mp4 file for site17 using AVPlayerViewController.

--- Cottontown/YouTubeContentViewController.swift
+++ Cottontown/YouTubeContentViewController.swift
@@ -16,12 +16,14 @@ class YouTubeContentViewController: UIViewController, UIScrollViewDelegate {
     var maxPages = 0
     var youTubeID:String = ""
     var youTubeText:String = ""
-    var stopPageVC: StopPageViewController?
+    
     weak var delegate: PictureContentViewControllerDelegate?
 
-    @IBOutlet weak var player: YTPlayerView!
-
+    @IBOutlet weak var playButton: UIButton!
+    
     @IBOutlet weak var youTubeTextField: UITextView!
+    
+    @IBOutlet weak var videoThumbnail: UIImageView!
     
     @IBOutlet weak var pageControl: UIPageControl!
     
@@ -31,27 +33,45 @@ class YouTubeContentViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        playButton.accessibilityLabel = "Plays video for stop detail page \(pageIndex + 1) of \(maxPages).  Swipe right for description"
+        
         youTubeTextField.text = youTubeText
         
         pageControl.currentPage = pageIndex
         pageControl.numberOfPages = maxPages
+        pageControl.currentPageIndicatorTintColor = UIColor.init(colorLiteralRed: 248.0/255, green: 210.0/255.0, blue: 103.0/255.0, alpha: 1.0)
 
 
-        let playVars = ["rel": 0,
-                        "showinfo": 0,
-                        "modestbranding": 0,
-                        "playsinline": 0,
-                        ]
-        
-        player.loadWithVideoId(youTubeID, playerVars: playVars)
+//        let playVars = ["rel": 0,
+//                        "showinfo": 0,
+//                        "modestbranding": 0,
+//                        "playsinline": 0,
+//                        ]
+//        
+//        player.loadWithVideoId(youTubeID, playerVars: playVars)
         
         leftArrow.hidden = false
+        leftArrow.accessibilityLabel = "Goes to page \(pageIndex)"
     }
     
+ 
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        delay(0.25) {
+            UIAccessibilityPostNotification(UIAccessibilityLayoutChangedNotification, self.playButton)
+        }
+
+    }
     override func viewDidLayoutSubviews() {
         youTubeTextField.setContentOffset(CGPointZero, animated: false)
         
+        let maxWidth = videoThumbnail.frame.width
+        
+        StopsModel.resizeImage(fileName: youTubeID + "_tn", type: "jpg", maxPointSize: maxWidth) { (image) in
+            self.videoThumbnail.image = image
+        }
     }
     
     deinit {
@@ -72,14 +92,13 @@ class YouTubeContentViewController: UIViewController, UIScrollViewDelegate {
     
     @IBAction func PlayYouTube(sender: UIButton) {
         
-        let path = NSBundle.mainBundle().pathForResource("Patti", ofType: "mp4")
+        let path = NSBundle.mainBundle().pathForResource(youTubeID, ofType: "mp4")
         let player = AVPlayer(URL: NSURL(fileURLWithPath: path!))
         let playerController = AVPlayerViewController()
         playerController.player = player
         presentViewController(playerController, animated: true) {
             player.play()
         }
-        
         
     }
     
